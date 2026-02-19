69f368fb5d31e29b6b2634c3bc4c82a133c00808
Liad Firouz <Liad.Firouz@gmail.com>
2026-02-19 11:06:07 +0200

    togglePermission and addUser functions was added

--- Exercies/Bit-Guard/main.c
+++ Exercies/Bit-Guard/main.c
@@ -9,3 +9,76 @@
 #define DELETE  0x08  // 00001000 (8)
 #define ADMIN   0x80  // 10000000 (128)
 
+// User Structer
+typedef struct User
+{
+    int id; //user id
+    char name [50]; // user name
+    unsigned char permissions; // user permissions
+    struct User* next; // pointer to the next use
+}User;
+
+// Functions
+void addUser(User** head);
+void togglePermission (User* head, int id, unsigned char mask);
+void printUsers(User* head);
+void deleteUser(User** head, int id);
+void freeList(User* head);
+
+int main(){
+
+    return 0;
+}
+
+void addUser(User** head){
+    // allocate new memory
+    User* newUser = (User*)malloc(sizeof(User));
+
+    // check if the pointer is point on the new memory
+    if(newUser == NULL){
+        printf("Error: Memory allocation failed!\n");
+        return;
+    }
+
+    // get user id
+    printf("Enter User ID: ");
+    scanf("%d", &newUser->id);
+
+    // clear the Enter from the last input
+    while (getchar() != '\n');
+    
+    // get the new new 
+    printf("Enter User Name:");
+    fgets(newUser->name, 50, stdin);
+
+    // remove "\n" from the name
+    newUser->name[strcspn(newUser->name, "\n")] = 0;
+
+    // point to the last person on the list
+    newUser->next = *head;
+
+    // point the start of the list to the new one
+    *head = newUser;
+}
+
+void togglePermission (User* head, int id, unsigned char mask){
+    
+    User *pointer = head;
+
+    while(pointer != NULL){
+        if(pointer->id == id){
+           pointer->permissions ^= mask;
+        }
+    }
+
+    printf("User with ID %d not found.\n", id);
+}
+
+void printUsers(User* head){
+    
+}
+
+
+
+
+
