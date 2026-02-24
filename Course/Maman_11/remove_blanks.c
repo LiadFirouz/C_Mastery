b54ac490eaede9563657afd72eb00e09ed0c1912
Liad Firouz <liad.firouz@gmail.com>
2026-02-24 10:13:37 +0200

    final q1

--- Course/Maman_11/remove_blanks.c
+++ Course/Maman_11/remove_blanks.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-#define STRING_LEN 50
+#define STRING_LEN 256
 
 void remove_blanks(char *string);
 
 int main(){
     char string [STRING_LEN];
 
-    while( fgets(string, STRING_LEN, stdin) != NULL){
+    while(fgets(string, STRING_LEN, stdin) != NULL){
         string[strcspn(string, "\n")] = '\0';
-        printf("The Input string:\n\"%s\"", string);
+        printf("The Input string:\n\"%s\"\n", string);
         remove_blanks(string);
-        printf("The string at the end of the function:\n\"%s\"", string);
+        printf("The string at the end of the function:\n\"%s\"\n", string);
     }
 
     return 0;
@@ -21,7 +21,7 @@ int main(){
 void remove_blanks(char *string){
     int writeIndex = 0, readIndex = 0;
 
-    printf("The string as received by the function:\n\"%s\"", string);
+    printf("The string as received by the function:\n\"%s\"\n", string);
 
     for(readIndex = 0; string[readIndex] != '\0'; readIndex++){
         if(!(string[readIndex] == ' ' || string[readIndex] == '\t' || string[readIndex] == '\n')){
