ab8e37c9d8eebc453b44fa06ed75aa938f84ead7
Liad Firouz <liad.firouz@gmail.com>
2026-03-04 12:03:46 +0200

    1.2

--- Course/Mamman_12/difference_and_sum.c
+++ Course/Mamman_12/difference_and_sum.c
@@ -6,29 +6,58 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define SUM(a, b) (((a) + (b)))
-#define DIFF(a,b)  (((a)>(b))?((a)-(b)) : ((b)-(a)))
+#define DIFF(a, b) (((a) > (b)) ? ((a) - (b)) : ((b) - (a)))
 
-void findDifferenceAndSum (int *result1, int *result2, int num1, int num2);
+void findDifferenceAndSum(int *diff, int *sum, int num1, int num2);
+void saveArray(int num1, int num2, int **arr, int *count);
 
-
-int main(){
-    int num1 = 0, num2 = 0, diff = 0, sum = 0;
-
+int main(void)
+{
+    int i = 0, num1 = 0, num2 = 0, diff = 0, sum = 0, count = 0;
+    int *arr = NULL;
 
     printf("Please enter 2 number for diffrence and sum:");
 
-    while(scanf("%d %d", &num1, &num2) != EOF){
-        printf("For %d, %d", num1, num2);
+    while (scanf("%d %d", &num1, &num2) != EOF)
+    {
+        printf("For %d, %d:\n", num1, num2);
+        saveArray(num1, num2, &arr, &count);
         findDifferenceAndSum(&diff, &sum, num1, num2);
+        printf("using function diff: %d, sum: %d\n", diff, sum);
+        printf("using macros diff: %d, sum: %d\n\n", DIFF(num1, num2), SUM(num1, num2));
     }
 
+    printf("The list:\n");
 
+    for (i = 0; i < count; i += 2)
+    {
+        printf("%d, %d\n", arr[i], arr[i + 1]);
+    }
+    free(arr);
     return 0;
 }
 
-void findDifferenceAndSum (int *diff, int *sum, int num1, int num2){
+void findDifferenceAndSum(int *diff, int *sum, int num1, int num2)
+{
     *sum = num1 + num2;
-    *diff = (num1 > num2)? (num1 - num2): (num2- num1);
+    *diff = (num1 > num2) ? (num1 - num2) : (num2 - num1);
+}
+
+void saveArray(int num1, int num2, int **arr, int *count)
+{
+    int *temp = (int *)realloc(*arr, (*count + 2) * sizeof(int));
+
+    if (temp != NULL)
+    {
+        *arr = temp;
+        (*arr)[*count] = num1;
+        (*arr)[*count + 1] = num2;
+        (*count) += 2;
+    }
+
+    else
+        printf("Error: Memory allocation failed!\n");
 }
